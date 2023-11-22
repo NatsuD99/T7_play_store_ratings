2c20ad7b16884a41ffa4a5452b55f0f040e21a6a
gguruksha <gguruksha@gmail.com>
2023-11-22 11:04:57 -0500

    basic changes commit

--- play_store_apps.py
+++ play_store_apps.py
@@ -17,9 +17,9 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
 # %%
-#df = pd.read_csv("google-playstore-apps/Google-Playstore.csv")
-#df.head()
-df.tail()
+df = pd.read_csv("google-playstore-apps/Google-Playstore.csv")
+df.head()
+# df.tail()
 # %% [markdown]
 # Data Explorartion
 # %%
@@ -53,3 +53,20 @@ df[df['App Name'].duplicated()][['App Name', 'App Id']].head(10)
 # So, we notice that there are no missing app IDs and none of the app IDs are duplicated as well.
 # There are multiple apps with same names but the ID is different, so it fine.
 # App Id, will thus be used as our primary key.
+
+#%%
+#Treating missing values for privacy col
+sample_values = df['Privacy Policy'].sample(n=20) #overview of sample 
+print(sample_values)
+df['Privacy Policy'].fillna('Nan', inplace=True)
+
+# %%
+#Treating missing values for Relesed col
+#replaced missing values with median date
+sample_values1 = df['Released'].sample(n=20)
+print(sample_values1)
+df['Released'] = pd.to_datetime(df['Released'], errors='coerce')
+median_date = df['Released'].median()  # Calculate the median date
+df['Released'].fillna(median_date, inplace=True)
+
+#%%
